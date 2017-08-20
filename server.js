c6900a4dd83214eb2107976668d592dd4232bbd8
G.Meghana <30664250+Radhakameswari@users.noreply.github.com>
2017-08-20 22:14:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ var content = {
     title: " Article One I Meghana",
     heading: "Article One",
     date: ' Aug 20, 2017',
-    
+    content: '<p>'
+                This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+            </p>
+             <p>
+                This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+            '</p>'
+             <p>
+                This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+           ' </p> '
 };
-
-
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
