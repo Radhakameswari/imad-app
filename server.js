1d5e9a8064c130cda5ee9480bf6640f6fe9638d9
G.Meghana <30664250+Radhakameswari@users.noreply.github.com>
2017-08-20 20:33:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var content = {
+    title: " Article One I Meghana",
+    heading: "Article One",
+    date: ' Aug 20, 2017',
+    content:
+};
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
