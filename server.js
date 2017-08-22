67a9a9cd2eeeef0be205277c14341dedaf6de8f2
G.Meghana <30664250+Radhakameswari@users.noreply.github.com>
2017-08-22 21:26:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,22 +6,12 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var content = ` {
+var content = `{
     title: " Article One I Meghana",
     heading: "Article One",
     date: ' Aug 20, 2017',
-    content: '     
-         <p>
-                This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>
-             <p>
-                This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>
-             <p>
-                This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>'
-   
-  `};
+    content: '  ',
+`};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
