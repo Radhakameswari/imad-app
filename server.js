44981c071c0d139a49a64b3eedc632bbb72c9785
G.Meghana <30664250+Radhakameswari@users.noreply.github.com>
2017-08-22 21:23:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,61 +10,25 @@ var content = ` {
     title: " Article One I Meghana",
     heading: "Article One",
     date: ' Aug 20, 2017',
-    content: ''
-     <p>  ;
-               This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.  
-          </p>
-             <p>  ;
-                 This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+    content: '     
+         <p>
+                This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
             </p>
              <p>
-                 This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
-        </p>
-      };
-function createTemplate (data) {
-   var title = data.title;
-   var heading = data.heading;
-   var date = data.date;
-   var content = data.content;
+                This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+            </p>
+             <p>
+                This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+            </p>'
    
-    var htmlTemplates = '
-        <html>
-          <head>
-            <title>
-                ${title}
-            </title>
-             <meta name="viewport" content="width-device-width, initial-scale=1" />
-               <link href="/ui/style.css" rel="stylesheet" />
-      
-          </head>
-            <body>
-              <div class="container">
-                <div>
-                  <a href="/">Home</a>
-              </div>
-              <hr/>
-             <h3>
-               ${heading}
-              </h3>
-              <div>
-              ${date}
-               </div>
-              <div>
-               ${content}
-           </div>
-             </div>
-          </body>
-      </html>
-        ' ;
-        return htmlTemplate;
-};
-  
+  `};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function  (req, res){
-    res.send(createTemplate(articleone));
+     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function  (req, res){
@@ -82,6 +46,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
