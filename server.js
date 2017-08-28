fedfa87454e7a0b093cd38a2c0f79f2943a1b3cc
G.Meghana <30664250+Radhakameswari@users.noreply.github.com>
2017-08-28 23:19:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,76 +4,146 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-
-var articleOne ; `{
-    title: " Article One I Meghana",
-    heading: "Article One",
-    date: ' Aug 20, 2017',
-    content: '              <p>
-                This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+var articles = 
+{
+     'article-one'   : {
+    title: 'Article One - Shady Dude',
+    heading: 'Article One',
+    date: "Aug 5. 2017",
+    content: `
+            <p>
+                    This is The Content For My First Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
             </p>
-             <p>
-                This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+            
+            <p>
+                    This is The Content For My First Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
             </p>
-             <p>
-                This is the content for my first article.  This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
-            </p>'
-}`;
-
+            
+            <p>
+                    This is The Content For My First Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>`
+                        },
+     
+     'article-two'   : {
+    title: 'Article Two - Shady Dude',
+    heading: 'Article Two',
+    date: "Aug 5. 2017",
+    content: `
+            <p>
+                    This is The Content For My Second Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My Second Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My Second Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>`
+                        },
+     
+     'article-three' : {
+    title: 'Article Three - Shady Dude',
+    heading: 'Article Three',
+    date: "Aug 5. 2017",
+    content: `
+            <p>
+                    This is The Content For My Third Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My Third Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>
+            
+            <p>
+                    This is The Content For My Third Article. Alrighty ? I'mma Copy This A Bunch Of Times So Don't Go Reading Everything Again And Again Till You Go Crazy. Wait, Have You Already Gone Crazy ? Uh . . . Huh I Don't Think So Righty ? Then . . . Bye Bye ! *Wave*  . . . . . . . . . 
+            </p>`
+         
+                        },
+};
 
 function createTemplate (data) {
-   var title = data.title;
-   var heading = data.heading;
-   var date = data.date;
-   var content = data.content; 
-   
-var htmlTemplate = `
+
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+
+<!DOCTYPE html>
 <html>
     <head>
         <title>
-           ${title}
-    </title>
-      <meta name="viewport" content="width-device-width, initial-scale=1" />
-      <link href="/ui/style.css" rel="stylesheet" />
-      
- </head>
-            <body>
-    <div class="container">
+            ${title}
+        </title>
+        <meta name="viewport" consent="width=device-width, initial-scale=1" />
+        <!--Above Line Is Used For Adjusting Width Of Screen According To Display-->
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    
+    <body>
+        <div class="container">
             <div>
-            <a href="/">Home</a>
+                <a href='/'>Home</a>
             </div>
             <hr/>
             <h3>
                 ${heading}
             </h3>
             <div>
-               ${date}
+                ${date}
             </div>
             <div>
-             ${content}
-        </div>
+               ${content}
+            </div>
         </div>
     </body>
+
 </html>
-`;
-    }
+                        `;
+    return htmlTemplate;
+
+
+}
+
+//This Is An Icon File I Uploaded Inside The /ui Folder. You Might Search For .ico(Icon) Files And Download, Change It's Name To 'favicon.ico 'And Upload It.
+app.get('/favicon.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+});
+
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+      counter = counter + 1;
+      res.send(counter.toString());
+});
+
+var names = [];
+app.get('/submit-name', function(req, res) { //URL : /submit-name?name=xxxxxx
+    //Get The Names From Request Object
+    var name = req.query.name;
+    
+    names.push(name);
+    // JSON : JavaScript Object Notation
+    res.send(JSON.stringify(names));
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function  (req, res){
-     res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req, res) {
+    //articleName == article-one
+    //articles[articleName] == {} content object for article
+    var articleName = req.params.articleName;
+	res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function  (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/article-three', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
